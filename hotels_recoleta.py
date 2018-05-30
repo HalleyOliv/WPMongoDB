7b358d36c1701b95ccf81c2dc14ffbaa17aa10f4
Halley Pacheco de Oliveira <31369199+HalleyOliv@users.noreply.github.com>
2018-05-30 17:19:41 -0300

    Update hotels_recoleta.py

--- hotels_recoleta.py
+++ hotels_recoleta.py
@@ -5,8 +5,42 @@ import numpy as np
 from pymongo import MongoClient
 
 # Connect to reficio database on MongoDB server
-conn = MongoClient('localhost', 27017)
-db = conn.reficio
+def Database():
+    server = '192.168.0.6'
+    port = 27017
+    conn = MongoClient(server, port)
+    return conn.reficio
+
+db = Database()
+
+# GPX heading
+
+gpx = open("hotels_recoleta.gpx", "w")
+gpx.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n")
+gpx.write("<gpx version=\"1.1\" " + \
+          "creator=\"Reficio - http://reficio.cc\" " + \
+          "xmlns=\"http://www.topografix.com/GPX/1/1\" " + \
+          "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " + \
+          "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 " + \
+          "http://www.topografix.com/GPX/1/1/gpx.xsd\">\n")
+gpx.write("\t<metadata>\n")
+gpx.write("\t\t<name>Hotels in Buenos Aires in the neighborhood of Recoleta</name>\n")
+gpx.write("\t\t<desc>Buenos Aires Data + Geonames</desc>\n")
+gpx.write("\t\t<author>\n")
+gpx.write("\t\t\t<name>Halley Pacheco de Oliveira</name>\n")
+gpx.write("\t\t\t<email id=\"halleypo\" domain=\"gmail.com\" />\n")
+gpx.write("\t\t\t<link href=\"http://reficio.cc/\">\n")
+gpx.write("\t\t\t\t<text>Reficio</text>\n")
+gpx.write("\t\t\t</link>\n")
+gpx.write("\t\t</author>\n")
+gpx.write("\t</metadata>\n")
+
+def wpt(name, desc, lat, lon):
+    gpx.write("\t<wpt lat=\"" + lat + "\" lon=\"" + lon + "\">\n")
+    gpx.write("\t\t<name>" + name + "</name>\n")
+    gpx.write("\t\t<desc>" + desc + "</desc>\n")
+    gpx.write("\t</wpt>\n")
+    return
 
 # Geometry of the neighborhood of Recoleta
 # from the barrios_porteños collection
@@ -29,8 +63,13 @@ for geoname in geonamesar.find( \
 }, \
 { "features.properties.feature_code" : "HTL" } \
 ]  \
-}, {"features.properties.name": 1, "_id" : 0} ):
-    hotels.append(np.array(geoname['features'][0]['properties']['name']))
+} ):
+    geonameid = geoname['features'][0]['properties']['geonameid']
+    name = geoname['features'][0]['properties']['name']
+    lat =  geoname['features'][0]['geometry']['coordinates'][1]
+    lon =  geoname['features'][0]['geometry']['coordinates'][0]
+    hotels.append(np.array(name))
+    wpt(name, geonameid, str(lat), str(lon))
 
 # Print the hotels in alphabetical order
 for hotel in sorted(hotels):
